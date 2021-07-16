8242f46ed72839567456a00ea8aca0758c61bcb3
George Wort <george.wort@arm.com>
2021-07-16 11:56:48 +0100

    Add Licence to state_compress and bitutils.
    Change-Id: I958daf82e5aef5bd306424dcfa7812382b266d65

--- src/util/state_compress.c
+++ src/util/state_compress.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2017, Intel Corporation
+ * Copyright (c) 2021, Arm Limited
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
