a86d6c290de5eeee73ebebd387e26b7705c65a75
apostolos <apostolos.tapsas@vectorcamp.gr>
2021-09-14 16:01:32 +0300

    nit

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -13,14 +13,12 @@ int main(){
     std::function<void(int,int,int,bool)>  functions[] = { shufti_benchmarks, rshufti_benchmarks, truffle_benchmarks, rtruffle_benchmarks };
     int sizes[] =  {  16000,  32000,  64000, 120000, 1600000, 2000000, 2500000, 3500000, 150000000, 250000000, 350000000, 500000000 };
     const char charset[] = "aAaAaAaAAAaaaaAAAAaaaaAAAAAAaaaAAaaa"; 
-    /*
     for (size_t i = 0; i < std::size(sizes); i++) {
         for(int j = 0; j < 4; j++) {
             functions[j](sizes[i], MAX_LOOPS / sizes[i], MAX_MATCHES, false);
             functions[j](sizes[i], MAX_LOOPS / sizes[i], MAX_MATCHES, true);  
         } 
     }
-    */
     for(size_t i=0; i < std::size(sizes); i++){
         //we imitate the noodle unit tests
         for (int char_len = 1; char_len < 9; char_len++) {
