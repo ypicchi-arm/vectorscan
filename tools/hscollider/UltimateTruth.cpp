773d57d890d9683fdb02f060603c67227c949f92
G.E. <gregory.economou@vectorcamp.gr>
2024-04-11 09:49:43 +0300

    added copyright info for modified src files

--- tools/hscollider/UltimateTruth.cpp
+++ tools/hscollider/UltimateTruth.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2019, Intel Corporation
+ * Copyright (c) 2024, VectorCamp PC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
